7688abbda4b87e6df5a245e5bd4417767257c656
azabraao <az.abraao@gmail.com>
2021-01-14 17:19:03 -0300

    add service graphql query to admin

--- react/AdminExample.tsx
+++ react/AdminExample.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react'
 import { Layout, PageBlock } from 'vtex.styleguide'
+import { useQuery } from 'react-apollo'
+
+import helloworld from './graphql/helloworld.gql'
 
 const AdminExample: FC = () => {
+  const { data } = useQuery(helloworld)
+
   return (
     <Layout>
-      <PageBlock title="Titulo" subtitle="Alguma explicação." variation="full">
+      <PageBlock title="Title" subtitle="Some explanation." variation="full">
         <h1>Hello, World!</h1>
+        <p>{data?.helloworld}</p>
       </PageBlock>
     </Layout>
   )
